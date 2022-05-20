a83979e4715ffd166da2255d0bb53164e506c173
Jarret Jeter <jarretjeter@yahoo.com>
2022-05-20 15:13:51 -0700

    create dataset with bigquery in main.py, add to requirements.txt

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+from google.cloud import bigquery
+import logging
+from logging import INFO
 import pandas as pd
+import pandas_gbq
+import sys
+
+logging.basicConfig(format='[%(levelname)-5s][%(asctime)s][%(module)s:%(lineno)04d] : %(message)s',
+                    level=INFO,
+                    stream=sys.stderr)
+logger: logging.Logger = logging
+
 
 class DataFrameLoader():
 
     def __init__(self, filepath):
         df = pd.read_csv(filepath, header=0)
         self.df = df
-        return df
-
 
     def head(self):
         df = self.df
@@ -34,8 +43,22 @@ class DataFrameLoader():
 
 
 rmn_emprs = DataFrameLoader("./data/roman-emperors.csv")
-
 rmn_emprs.dropna()
 rmn_emprs.drop_duplicates()
 rmn_emprs.fillna()
 rmn_emprs.sort_values("Index")
+
+
+
+# project_id = "deb-01-346205"
+client = bigquery.Client()
+
+dataset_id = "{}.roman_emperors".format(client.project)
+
+dataset = bigquery.Dataset(dataset_id)
+
+dataset.location = "US"
+
+dataset = client.create_dataset(dataset, timeout=30)
+
+logger.info("Created dataset {}.{}".format(client.project, dataset.dataset_id))
