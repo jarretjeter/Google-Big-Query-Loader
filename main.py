ab5d6fd8e374211fcbeb56b463996f453ec2d5b4
Jarret Jeter <jarretjeter@yahoo.com>
2022-05-24 22:31:44 -0700

    remove unused/non-working code. Add comments to code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from google.cloud import bigquery
 import logging
 from logging import INFO
-import os
 import pandas as pd
 import pandas_gbq
 import sys
@@ -11,92 +10,39 @@ logging.basicConfig(format='[%(levelname)-5s][%(asctime)s][%(module)s:%(lineno)0
                     stream=sys.stderr)
 logger: logging.Logger = logging
 
-
-class DataFrameLoader():
-
-    def __init__(self, filepath):
-        df = pd.read_csv(filepath, header=0)
-        self.df = df
-
-    def head(self):
-        df = self.df
-        return df.head()
-
-    def tail(self):
-        df = self.df
-        return df.tail()
-
-    def dropna(self):
-        df = self.df
-        return df.dropna(how="all", axis="columns", inplace=True)
-
-    def drop_duplicates(self):
-        df = self.df
-        return df.drop_duplicates(inplace=True)
-
-    def fillna(self):
-        df = self.df
-        return df.fillna(value=0, inplace=True)
-
-    def sort_values(self, col_name):
-        df = self.df
-        return df.sort_values(by=col_name, inplace=True)
-
-    def dtypes(self):
-        df = self.df
-        return df.dtypes
-
-    def items(self):
-        df = self.df
-        for label, content in df.items():
-            return f"label: {label}", f"content: {content}"
-
-
-# rmn_emprs = DataFrameLoader("./data/roman-emperors.csv")
-# rmn_emprs.dropna()
-# rmn_emprs.drop_duplicates()
-# rmn_emprs.fillna()
-# rmn_emprs.sort_values("Index")
+# Reading csv into dataframe and performing transformations, setting inplace=True for each so that no copies are made for this case.
 rmn_emprs = pd.read_csv("./data/roman-emperors.csv")
+# Column names cannot have white spaces, replacing with underscores
 rmn_emprs.rename(columns={"Full Name": "Full_Name", "Birth City": "Birth_City", "Birth Province": "Birth_Province", "Reign Start": "Reign_Start", "Reign End": "Reign_End"}, inplace=True)
+# Dropped the unnecessary Unnamed columns that also had NaN values
 rmn_emprs.dropna(how="all", axis="columns", inplace=True)
+# Drop any potential duplicate rows(there were none)
 rmn_emprs.drop_duplicates(inplace=True)
+# The Verif column had a lot of NaN values, so I changed them to 0
 rmn_emprs.fillna(value=0, inplace=True)
+# Returning the dataframe sorted by the Index column orders the emperors first ruler to last. Sorting by Reign_Start isn't quite correct because the date doesn't take into account the years 26 "BC" to 14 "AD" for the ruler Augustus
 rmn_emprs.sort_values(by="Index", inplace=True)
 
 
-# credential_path = "/home/jarret/google-creds/deb-01-346205-5113c43e514f.json"
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credential_path
-
-# class BQLoader():
-
-#     def __init__(self):
-#         client = bigquery.Client()
-#         self.client = client 
-    
-#     def create_dataset(self, id:str, location:str) -> None:
-#         client = self.client
-#         dataset_id = "{}." + f"{id}".format(client.project)
-#         dataset = bigquery.Dataset(dataset_id)
-#         dataset.location = f"{location}"
-#         dataset = client.create_dataset(dataset, timeout=30)
-#         logger.info("Created dataset {}.{}".format(client.project, dataset.dataset_id))
-
-# bq_dataset = BQLoader()
-# bq_dataset.create_dataset("bq_code_review", "us-west2")
-
-
+# Instantiate my big query client api which will create a dataset
 client = bigquery.Client()
+# Tell the client to use "bq_code_review" as the dataset name for my project
 dataset_id = "{}.bq_code_review".format(client.project)
+# Pass dataset_id to bigquery's Dataset class to build a reference
 dataset = bigquery.Dataset(dataset_id)
+# Assign the datasets server location to us-west2
 dataset.location = "us-west2"
-dataset = client.create_dataset(dataset, timeout=30, exists_ok=True)
+# Tell my client to create the dataset on google big query with the completed information
+dataset = client.create_dataset(dataset, timeout=30)
+# If successful, log the creation of the dataset
 logger.info("Created dataset {}.{}".format(client.project, dataset.dataset_id))
 
-
+# Project to look for when creating a table,
 project_id = "deb-01-346205"
+# dataset to insert the table into
 table_id = "bq_code_review.roman_emperors"
 
+# Loading transformed dataframe into google big query with the specified project/dataset as targets and a specified table schema. load_parquet was the default api_method, and was giving me errors.
 pandas_gbq.to_gbq(rmn_emprs, table_id, project_id=project_id, if_exists="replace", api_method="load_csv", table_schema=[
     {'name': 'Index', 'type': 'INT64'}, 
     {'name': 'Name', 'type': 'STRING'}, 
